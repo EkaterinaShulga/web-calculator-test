c318f04837f017cc7638b2efe4b4133b4f6015c6
EkaterinaShulga <shulga23@gmail.com>
2022-07-11 22:01:15 +0300

    параметризированные тесты исправила

--- src/test/java/pro/sky/java/course3/webcalculatortest/CalculatorServiceImplParameterTest.java
+++ src/test/java/pro/sky/java/course3/webcalculatortest/CalculatorServiceImplParameterTest.java
@@ -12,42 +12,70 @@ public class CalculatorServiceImplParameterTest {
 
     private final CalculatorServiceImpl calculatorServiceImpl = new CalculatorServiceImpl();
 
-    @MethodSource("giveArgumentsForCalculatorTests")
+    @MethodSource("giveArgumentsForCalculatorTestsSum")
     @ParameterizedTest
-    public void sum(double num1, double num2) {
-        Assertions.assertEquals(num1 + num2, calculatorServiceImpl.returnAmount(num1, num2));
+    public void sum(double num1, double num2, double expected) {
+        Assertions.assertEquals(expected, calculatorServiceImpl.returnAmount(num1, num2));
 
     }
 
-    @MethodSource("giveArgumentsForCalculatorTests")
+    @MethodSource("giveArgumentsForCalculatorTestsMinus")
     @ParameterizedTest
-    public void difference(double num1, double num2) {
+    public void minus(double num1, double num2, double expected) {
         Assertions.assertEquals(num1 - num2, calculatorServiceImpl.returnDifference(num1, num2));
     }
 
-    @MethodSource("giveArgumentsForCalculatorTests")
+    @MethodSource("giveArgumentsForCalculatorTestsMultiplication")
     @ParameterizedTest
-    public void multiplication(double num1, double num2) {
-        Assertions.assertEquals(num1 * num2, calculatorServiceImpl.returnMultiplication(num1, num2));
+    public void multiplication(double num1, double num2, double expected) {
+        Assertions.assertEquals(expected, calculatorServiceImpl.returnMultiplication(num1, num2));
     }
 
-    @MethodSource("giveArgumentsForCalculatorTests")
+    @MethodSource("giveArgumentsForCalculatorTestsDivide")
     @ParameterizedTest
-    public void divide(double num1, double num2) {
-        Assertions.assertEquals(num1 / num2, calculatorServiceImpl.returnDivision(num1, num2));
+    public void divide(double num1, double num2, double expected) {
+        Assertions.assertEquals(expected, calculatorServiceImpl.returnDivision(num1, num2));
     }
 
-    private static Stream<Arguments> giveArgumentsForCalculatorTests() {
+    private static Stream<Arguments> giveArgumentsForCalculatorTestsSum() {
         return Stream.of(
-                Arguments.of(3.0, 1.0),
-                Arguments.of(5.0, 7.0),
-                Arguments.of(10.0, 3.0),
-                Arguments.of(20.0, 5.0),
-                Arguments.of(-20.0, 4.0),
-                Arguments.of(-20.0, 6.0)
+                Arguments.of(3.0, 1.0, 4.0),
+                Arguments.of(7.0, 5.0, 12.0),
+                Arguments.of(10.0, 3.0, 13.0),
+                Arguments.of(20.0, 5.0, 25.0),
+                Arguments.of(-20.0, 4.0, -16.0)
 
         );
     }
 
+    private static Stream<Arguments> giveArgumentsForCalculatorTestsMinus() {
+        return Stream.of(
+                Arguments.of(3.0, 1.0, 2.0),
+                Arguments.of(7.0, 5.0, 2.0),
+                Arguments.of(10.0, 3.0, 7.0),
+                Arguments.of(20.0, 5.0, 15.0),
+                Arguments.of(-20.0, 4.0, -16.0)
+        );
+    }
+
+    private static Stream<Arguments> giveArgumentsForCalculatorTestsMultiplication() {
+        return Stream.of(
+                Arguments.of(3.0, 1.0, 3.0),
+                Arguments.of(7.0, 5.0, 35.0),
+                Arguments.of(10.0, 3.0, 30.0),
+                Arguments.of(20.0, 5.0, 100.0),
+                Arguments.of(-20.0, 4.0, -80.0)
+        );
+    }
+
+    private static Stream<Arguments> giveArgumentsForCalculatorTestsDivide() {
+        return Stream.of(
+                Arguments.of(3.0, 1.0, 3.0),
+                Arguments.of(70.0, 5.0, 14.0),
+                Arguments.of(9.0, 3.0, 3.0),
+                Arguments.of(20.0, 5.0, 4.0),
+                Arguments.of(-20.0, 4.0, -5.0)
+        );
+    }
 
 }
